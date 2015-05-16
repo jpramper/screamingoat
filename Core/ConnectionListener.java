3855ae9fbe26203c9a9bfc5925d579e34f0b6e4a
John <jp.ramper@gmail.com>
2015-05-15 20:52:32 -0500

    connlistener v4

--- Core/ConnectionListener.java
+++ Core/ConnectionListener.java
@@ -55,13 +55,30 @@ public class ConnectionListener implements Runnable {
 			case 13: // who listens?
 				if (Global.isServer) {
 					// if i'm the server, send a response on the ACK channel
-					Global.sendMessage(
-							14, 
-							"", 
-							address, 
-							0, 
-							Global.ackSocket, 
-							Global.ackPort);
+					DataPacket p = new DataPacket();
+					p.dataType = 14;
+					p.data = "";
+					p.destType = 0;
+					p.serverIp = Global.serverIp.toString().replace("/", "");
+
+					// create a UDP packet with the data packet
+					DatagramPacket sendPacket = null;
+					try {
+						sendPacket = new DatagramPacket(
+								p.toString().getBytes(), 
+								p.toString().length(), 
+								InetAddress.getByName(address.replace("/", "")), 
+								Global.ackPort);
+					} catch (UnknownHostException e) {
+						e.printStackTrace();
+					}
+					
+					// send the packet
+				    try {
+						Global.ackSocket.send(sendPacket);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
 				}
 				break;
 				
@@ -78,13 +95,6 @@ public class ConnectionListener implements Runnable {
 			case 15: 
 				if (Global.isServer) {
 					// if i'm the server, send a response on the ACK channel
-					Global.sendMessage(
-							15, 
-							"", 
-							address, 
-							0, 
-							Global.ackSocket, 
-							Global.ackPort);
 					DataPacket p = new DataPacket();
 					p.dataType = 15;
 					p.data = "";
